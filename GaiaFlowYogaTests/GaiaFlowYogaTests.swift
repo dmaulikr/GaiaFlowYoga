c46f4fb1fceea0f82c7ea673663ca264fb5840d3
John Hurdle <jhurd23@gmail.com>
2016-09-28 16:19:01 -0500

    Initial Commit

---
+++ GaiaFlowYogaTests/GaiaFlowYogaTests.swift
@@ -0,0 +1,36 @@
+//
+//  GaiaFlowYogaTests.swift
+//  GaiaFlowYogaTests
+//
+//  Created by John Hurdle on 9/28/16.
+//  Copyright © 2016 John Hurdle. All rights reserved.
+//
+
+import XCTest
+@testable import GaiaFlowYoga
+
+class GaiaFlowYogaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
